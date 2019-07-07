779385e51ec1300d4e1e250b69bb045e7a8ab55a
Никита <jubin2la@mail.ru>
2019-07-07 16:39:58 +0300

    Программа работает с постгресс таблцей

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -5,22 +5,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
-public class Messege {
+public class Message {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
+
     private String text;
-    private Integer tag;
+    private String tag;
 
-    public void setId(Integer id) {
-        this.id = id;
+    public Message() {
     }
 
-    public void setText(String text) {
+    public Message(String text, String tag) {
         this.text = text;
-    }
-
-    public void setTag(Integer tag) {
         this.tag = tag;
     }
 
@@ -28,11 +25,24 @@ public class Messege {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getText() {
         return text;
     }
 
-    public Integer getTag() {
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
         return tag;
     }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
+
