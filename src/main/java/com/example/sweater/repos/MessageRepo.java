f8daca2d683c81907c60eac4fd7f960984ac3257
Никита <jubin2la@mail.ru>
2019-11-04 21:32:05 +0300

    vvv

--- src/main/java/com/example/sweater/repos/MessageRepo.java
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
    List<Message> findByTag(String tag);
-//fdfdf
+//fdfdfdvdv
 }
 //Репозиторий позволяет нам получить полный список полей, либо получить список полей по идентификатору
