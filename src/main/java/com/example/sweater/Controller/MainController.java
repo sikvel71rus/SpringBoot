4f4d41e05017eb6c8ff8e56a2be0debb34ce7cfc
NYUbin <Nyubin@IBS.ru>
2019-12-25 17:11:37 +0300

    add registration

--- src/main/java/com/example/sweater/Controller/MainController.java
+++ src/main/java/com/example/sweater/Controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.Controller;
 import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +12,21 @@ import java.util.Map;
 
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
-    @PostMapping("")
+    @PostMapping("/main")
     public String add(@RequestParam String text,@RequestParam String tag, Map<String, Object> model){
         //Сохранили
         Message message = new Message(text, tag);
