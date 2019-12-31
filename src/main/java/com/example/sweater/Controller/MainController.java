8d312dd9605d0cdd615cf3e1956cf170ac8f99d5
NYUbin <Nyubin@IBS.ru>
2019-12-31 22:21:27 +0300

    add ftl

--- src/main/java/com/example/sweater/Controller/MainController.java
+++ src/main/java/com/example/sweater/Controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.sweater.Controller;
 import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +29,12 @@ public class MainController {
         return "main";
     }
     @PostMapping("/main")
-    public String add(@RequestParam String text,@RequestParam String tag, Map<String, Object> model){
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model){
         //Сохранили
-        Message message = new Message(text, tag);
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         //Конец сохранения
         //Взяли из репозитория, положили в модель и отдали пользователю
@@ -37,7 +42,7 @@ public class MainController {
         model.put("messages", messages);
         return "main";
     }
-    @PostMapping("filter")
+    @PostMapping("/filter")
     public String filter(@RequestParam String filter, Map<String, Object> model){
         Iterable<Message> messages;
         if (filter != null && !filter.isEmpty()){
@@ -49,9 +54,9 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("delete")
-    public String delete(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    @PostMapping("/delete")
+    public String delete(@RequestParam String text, Map<String, Object> model){
+        Message message = messageRepo.findById(Integer.valueOf(text));
         messageRepo.delete(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
